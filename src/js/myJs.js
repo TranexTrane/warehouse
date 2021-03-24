7985f6464dcd8f8e4595f82a40e2d18f3ff55064
TranexTrane <denoncinedouard@hotmail.be>
2021-03-24 18:15:42 +0100

    Push de 18h15 le 24-03-21

--- src/js/myJs.js
+++ src/js/myJs.js
@@ -1,2 +1,21 @@
-var hello = "hello";
-console.log(`${hello} world`);
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+  let navigation = document.querySelector(".main-nav");
+        let scrollGoal = (window.innerHeight) / 5;
+        let dernierScroll = 0;
+        window.addEventListener('scroll', function() {
+        let alreadyScrolled = window.pageYOffset;
+        if(alreadyScrolled > scrollGoal) {
+            navigation.classList.add("hide");
+        } else {
+            navigation.classList.remove("hide");
+        }
+        if (alreadyScrolled < dernierScroll) {
+            navigation.classList.remove("hide");
+        }
+        dernierScroll = alreadyScrolled;
+        });
